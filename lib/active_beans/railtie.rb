efcc74737872eda3822b784e1c18015e7e03bea9
Sergiy Volkov <sv@mooby.org>
2012-01-16 20:00:43 +0200

    add patched actionview

--- lib/active_beans/railtie.rb
+++ lib/active_beans/railtie.rb
@@ -13,3 +13,17 @@ module ActiveBeans
     end
   end
 end
+
+module ActionView
+  class TemplateRenderer < AbstractRenderer
+    def render(context, options)
+      @view = context
+      ActiveBeans.syncronize!
+      wrap_formats(options[:template] || options[:file]) do
+        template = determine_template(options)
+        freeze_formats(template.formats, true)
+        render_template(template, options[:layout], options[:locals])
+      end
+    end
+  end
+end
